aef05b7edd147c4a5c344b4a6d7253be0b4a1a10
theoBIGBIZYOU <theo@bigbizyou.agency>
2023-04-21 09:41:33 +0200

    add perso call api

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"VOIR": "/docs"}
+
+@app.get("/news/?t={title}&c={content}")
+def read_item(title: Union[str, None] = None, content: Union[str, None] = None):
+    return {"title": title,"content": content}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/news/{title}")
+def read_item(title: Union[str, None] = None):
+    return {"title": title}
+
+@app.get("/news/{content}")
+def read_item(content: Union[str, None] = None):
+    return {"content": content}
